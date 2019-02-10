74c11e79752109441079875c9b7c80209dd7dfb9
Eric <eric_gomez_1@hotmail.com>
2019-02-10 10:23:35 -0800

    boilerplate set up

---
+++ src/main/java/app/spring5webapp/model/Author.java
@@ -0,0 +1,12 @@
+package app.spring5webapp.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Author
+{
+    private String fname;
+    private String lname;
+
+
+}
---
+++ src/main/java/app/spring5webapp/model/Book.java
@@ -0,0 +1,9 @@
+package app.spring5webapp.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book
+{
+    private Author author;
+}
