1afcbacba446eb239a9012a23cda4094d917eb92
Eric <eric_gomez_1@hotmail.com>
2019-02-10 10:30:31 -0800

    filled in both jpa entitiy files.

--- src/main/java/app/spring5webapp/model/Author.java
+++ src/main/java/app/spring5webapp/model/Author.java
@@ -1,6 +1,8 @@
 package app.spring5webapp.model;
 
 import javax.persistence.Entity;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Author
@@ -8,5 +10,53 @@ public class Author
     private String fname;
     private String lname;
 
+    private Set<Book> books = new HashSet<>();
 
+    public Author()
+    {
+
+    }
+
+    public Author(String fname, String lname)
+    {
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public Author(String fname, String lname, Set<Book> books)
+    {
+        this.fname = fname;
+        this.lname = lname;
+        this.books = books;
+    }
+
+    public String getFname()
+    {
+        return fname;
+    }
+
+    public void setFname(String fname)
+    {
+        this.fname = fname;
+    }
+
+    public String getLname()
+    {
+        return lname;
+    }
+
+    public void setLname(String lname)
+    {
+        this.lname = lname;
+    }
+
+    public Set<Book> getBooks()
+    {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books)
+    {
+        this.books = books;
+    }
 }
